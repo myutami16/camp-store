fdcd5bf31e32944526a94b2544d8c35de2185dc3
myutami16 <mardhiyatul.utami@gmail.com>
2025-05-09 23:11:22 +0700

    feat: setup database and cloudinary

--- api/test/index.js
+++ api/test/index.js
@@ -1,15 +1,12 @@
-import { connectDB } from "../../lib/db";
+import { cloudinary } from "../../lib/cloudinary.js";
 
 export default async function handler(req, res) {
-	// Hanya izinkan metode GET untuk handler ini
-	if (req.method !== "GET") {
-		return res.status(405).json({ error: "Method Not Allowed" });
-	}
-
 	try {
-		await connectDB();
-		res.status(200).json({ message: "✅ MongoDB connected successfully!" });
-	} catch (err) {
-		res.status(500).json({ error: err.message });
+		const result = await cloudinary.uploader.upload(
+			"https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg"
+		);
+		res.status(200).json({ message: "Upload sukses", url: result.secure_url });
+	} catch (error) {
+		res.status(500).json({ message: "Upload gagal", error: error.message });
 	}
 }
